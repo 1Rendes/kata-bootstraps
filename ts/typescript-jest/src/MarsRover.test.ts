c98e244e42245c080ceb26ea0c47cf5a7d75702d
1Rendes <1admin@ukr.net>
2024-11-29 12:18:52 +0100

    3

--- ts/typescript-jest/src/MarsRover.test.ts
+++ ts/typescript-jest/src/MarsRover.test.ts
@@ -1,6 +1,17 @@
 import { Rover } from "./MarsRover";
 
 describe("MarsRover", () => {
+  it("dont move if target equal to initial position", () => {
+    const initialPosition = [0, 0];
+    const initialDirection = "N";
+    const target = [0, 0];
+    const expectedComands = [];
+    const rover = new Rover();
+
+    const comands = rover.getComands(initialPosition, initialDirection, target);
+
+    expect(comands).toEqual(expectedComands);
+  });
   it("return 'left' if current direction is N and target is on W", () => {
     const initialPosition = [1, 0];
     const initialDirection = "N";
@@ -8,7 +19,7 @@ describe("MarsRover", () => {
     const expectedComands = ["left"];
     const rover = new Rover();
 
-    const comands = rover.getComands(initialPosition, target, initialDirection);
+    const comands = rover.getComands(initialPosition, initialDirection, target);
 
     expect(comands).toEqual(expectedComands);
   });
@@ -19,7 +30,40 @@ describe("MarsRover", () => {
     const expectedComands = ["right"];
     const rover = new Rover();
 
-    const comands = rover.getComands(initialPosition, target, initialDirection);
+    const comands = rover.getComands(initialPosition, initialDirection, target);
+
+    expect(comands).toEqual(expectedComands);
+  });
+  it("return 'left,left' if current direction is N and target is on S", () => {
+    const initialPosition = [0, 1];
+    const initialDirection = "N";
+    const target = [0, 0];
+    const expectedComands = ["left", "left"];
+    const rover = new Rover();
+
+    const comands = rover.getComands(initialPosition, initialDirection, target);
+
+    expect(comands).toEqual(expectedComands);
+  });
+  it("return 'left,left' if current direction is S and target is on N", () => {
+    const initialPosition = [0, 0];
+    const initialDirection = "S";
+    const target = [0, 1];
+    const expectedComands = ["left", "left"];
+    const rover = new Rover();
+
+    const comands = rover.getComands(initialPosition, initialDirection, target);
+
+    expect(comands).toEqual(expectedComands);
+  });
+  it("return 'right' if current direction is W and target is on N", () => {
+    const initialPosition = [0, 0];
+    const initialDirection = "W";
+    const target = [0, 1];
+    const expectedComands = ["right"];
+    const rover = new Rover();
+
+    const comands = rover.getComands(initialPosition, initialDirection, target);
 
     expect(comands).toEqual(expectedComands);
   });
--- ts/typescript-jest/src/MarsRover.ts
+++ ts/typescript-jest/src/MarsRover.ts
@@ -1,12 +1,16 @@
 export class Rover {
   public getComands(
     initialPosition: number[],
-    target: number[],
-    initialDirection: string
+    initialDirection: string,
+    target: number[]
   ) {
     const [initX, initY] = initialPosition;
     const [targetX, targetY] = target;
+    if (initX === targetX && initY === targetY) return [];
     if (initX < targetX) return ["right"];
+    if (initY > targetY) return ["left", "left"];
+    if (initY < targetY && initialDirection === "W") return ["right"];
+    if (initY < targetY) return ["left", "left"];
     return ["left"];
   }
 }
