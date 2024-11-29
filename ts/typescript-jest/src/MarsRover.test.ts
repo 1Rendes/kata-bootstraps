f3eb4e8cfaa6148fbfeaa91e80b735050bb99968
1Rendes <1admin@ukr.net>
2024-11-29 11:31:49 +0100

    2

--- ts/typescript-jest/src/MarsRover.test.ts
+++ ts/typescript-jest/src/MarsRover.test.ts
@@ -2,11 +2,24 @@ import { Rover } from "./MarsRover";
 
 describe("MarsRover", () => {
   it("return 'left' if current direction is N and target is on W", () => {
-    const initialData = [[0, 0], "N"];
+    const initialPosition = [1, 0];
+    const initialDirection = "N";
+    const target = [0, 0];
     const expectedComands = ["left"];
     const rover = new Rover();
 
-    const comands = rover.getComands(initialData);
+    const comands = rover.getComands(initialPosition, target, initialDirection);
+
+    expect(comands).toEqual(expectedComands);
+  });
+  it("return 'right' if current direction is N and target is on E", () => {
+    const initialPosition = [0, 0];
+    const initialDirection = "N";
+    const target = [1, 0];
+    const expectedComands = ["right"];
+    const rover = new Rover();
+
+    const comands = rover.getComands(initialPosition, target, initialDirection);
 
     expect(comands).toEqual(expectedComands);
   });
--- ts/typescript-jest/src/MarsRover.ts
+++ ts/typescript-jest/src/MarsRover.ts
@@ -1,5 +1,12 @@
 export class Rover {
-  public getComands(initialData: (string | number[])[]) {
+  public getComands(
+    initialPosition: number[],
+    target: number[],
+    initialDirection: string
+  ) {
+    const [initX, initY] = initialPosition;
+    const [targetX, targetY] = target;
+    if (initX < targetX) return ["right"];
     return ["left"];
   }
 }
