71439d2c6c6a075b9fb7f194d24a2dacacffc39a
1Rendes <1admin@ukr.net>
2024-12-02 12:02:30 +0100

    5 test done

--- ts/typescript-jest/src/GameOfLife.test.ts
+++ ts/typescript-jest/src/GameOfLife.test.ts
@@ -23,4 +23,33 @@ describe("Game of life", () => {
 
     expect(numberOfAliveNeighbours).toBe(expectedNumberOfAliveNeighbours);
   });
+  it("cell can calculate alive neighbours", () => {
+    const initialNeighboursState = [0, 1, 0, 0, 0, 0, 0, 0];
+    const expectedNumberOfAliveNeighbours = 1;
+
+    const cell = new Cell();
+    const numberOfAliveNeighbours = cell.calculateNeighbours(
+      initialNeighboursState
+    );
+
+    expect(numberOfAliveNeighbours).toBe(expectedNumberOfAliveNeighbours);
+  });
+  it("cell become dead if 1 neighbour is alive", () => {
+    const initialNeighboursState = [0, 1, 0, 0, 0, 0, 0, 0];
+    const expectedCellState = 0;
+
+    const cell = new Cell();
+    const cellState = cell.defineState(initialNeighboursState);
+
+    expect(cellState).toBe(expectedCellState);
+  });
+  it("cell become dead if 2 neighbour is alive", () => {
+    const initialNeighboursState = [0, 1, 1, 0, 0, 0, 0, 0];
+    const expectedCellState = 1;
+
+    const cell = new Cell();
+    const cellState = cell.defineState(initialNeighboursState);
+
+    expect(cellState).toBe(expectedCellState);
+  });
 });
--- ts/typescript-jest/src/GameOfLife.ts
+++ ts/typescript-jest/src/GameOfLife.ts
@@ -1,4 +1,7 @@
 export class Cell {
+  defineState(initialNeighboursState: number[]) {
+    return this.calculateNeighbours(initialNeighboursState) > 1 ? 1 : 0;
+  }
   calculateNeighbours(initialNeighboursState: number[]) {
     return initialNeighboursState.filter((state) => state).length;
   }
