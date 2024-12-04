9dd6d9a80db088ff6e61fc8ec6241f536320fede
1Rendes <1admin@ukr.net>
2024-12-04 14:28:55 +0100

    all movements done

--- ts/typescript-jest/src/MarsRoverV2.test.ts
+++ ts/typescript-jest/src/MarsRoverV2.test.ts
@@ -1,22 +1,67 @@
 import { Rover } from "./MarsRoverV2";
 
 describe("RoverV2", () => {
-  it("should return [1,1,w] after turnLeft if initial position is [1,1,n]", () => {
-    const initialPosition = { x: 1, y: 2, d: "n" };
-    const expectedPosition = { x: 1, y: 2, d: "w" };
+  it("should return [1,1,w] after 'left' if initial position is [1,1,n]", () => {
+    const initialPosition = { x: 1, y: 1, d: "n" };
+    const expectedPosition = { x: 1, y: 1, d: "w" };
 
     const initRover = new Rover();
-    const roverAfterTurnLeft = initRover.turnLeft(initialPosition);
+    const roverAfterTurnLeft = initRover.left(initialPosition);
 
     expect(roverAfterTurnLeft).toEqual(expectedPosition);
   });
-  it("should return [1,1,e] after turnRight if initial position is [1,1,s]", () => {
-    const initialPosition = { x: 1, y: 2, d: "e" };
+  it("should return [1,1,e] after 'right' if initial position is [1,1,s]", () => {
+    const initialPosition = { x: 1, y: 1, d: "e" };
+    const expectedPosition = { x: 1, y: 1, d: "s" };
+
+    const initRover = new Rover();
+    const roverAfterTurnRight = initRover.right(initialPosition);
+
+    expect(roverAfterTurnRight).toEqual(expectedPosition);
+  });
+  it("should return [1,0,s] after 'forward' if initial position is [1,1,s]", () => {
+    const initialPosition = { x: 1, y: 1, d: "s" };
+    const expectedPosition = { x: 1, y: 0, d: "s" };
+
+    const initRover = new Rover();
+    const roverAfterForward = initRover.forward(initialPosition);
+
+    expect(roverAfterForward).toEqual(expectedPosition);
+  });
+  it("should return [1,2,n] after 'forward' if initial position is [1,1,n]", () => {
+    const initialPosition = { x: 1, y: 1, d: "n" };
+    const expectedPosition = { x: 1, y: 2, d: "n" };
+
+    const initRover = new Rover();
+    const roverAfterForward = initRover.forward(initialPosition);
+
+    expect(roverAfterForward).toEqual(expectedPosition);
+  });
+  it("should return [2,1,e] after 'forward' if initial position is [1,1,e]", () => {
+    const initialPosition = { x: 1, y: 1, d: "e" };
+    const expectedPosition = { x: 2, y: 1, d: "e" };
+
+    const initRover = new Rover();
+    const roverAfterForward = initRover.forward(initialPosition);
+
+    expect(roverAfterForward).toEqual(expectedPosition);
+  });
+  it("should return [0,1,e] after 'back' if initial position is [1,1,e]", () => {
+    const initialPosition = { x: 1, y: 1, d: "e" };
+    const expectedPosition = { x: 0, y: 1, d: "e" };
+
+    const initRover = new Rover();
+    const roverAfterBack = initRover.back(initialPosition);
+
+    expect(roverAfterBack).toEqual(expectedPosition);
+  });
+  it("should return [1,2,s] after 'back' if initial position is [1,1,s]", () => {
+    const initialPosition = { x: 1, y: 1, d: "s" };
     const expectedPosition = { x: 1, y: 2, d: "s" };
 
     const initRover = new Rover();
-    const roverAfterTurnLeft = initRover.turnLeft(initialPosition);
+    const roverAfterBack = initRover.back(initialPosition);
 
-    expect(roverAfterTurnLeft).toEqual(expectedPosition);
+    expect(roverAfterBack).toEqual(expectedPosition);
   });
 });
--- ts/typescript-jest/src/MarsRoverV2.ts
+++ ts/typescript-jest/src/MarsRoverV2.ts
@@ -1,5 +1,41 @@
 export class Rover {
-  public turnLeft(initPosition) {
-    return { d: "w", x: 1, y: 2 };
+  public forward(initPosition: { x: number; y: number; d: string }) {
+    const forward = 1;
+    return this.move(initPosition, forward);
+  }
+  public back(initPosition: { x: number; y: number; d: string }) {
+    const back = -1;
+    return this.move(initPosition, back);
+  }
+  private move(
+    initPosition: { x: number; y: number; d: string },
+    directionOfMoving: number
+  ) {
+    if (initPosition.d === "w")
+      return { ...initPosition, x: initPosition.x - directionOfMoving };
+    if (initPosition.d === "e")
+      return { ...initPosition, x: initPosition.x + directionOfMoving };
+    if (initPosition.d === "n")
+      return { ...initPosition, y: initPosition.y + directionOfMoving };
+    if (initPosition.d === "s")
+      return { ...initPosition, y: initPosition.y - directionOfMoving };
+  }
+  public right(initPosition: { x: number; y: number; d: string }) {
+    const numericSidesOfWorld = ["n", "w", "s", "e"];
+    const numericTurnRight = -1;
+    const newDirection =
+      numericSidesOfWorld[
+        numericSidesOfWorld.indexOf(initPosition.d) + numericTurnRight
+      ] || numericSidesOfWorld[numericSidesOfWorld.length - 1];
+    return { ...initPosition, d: newDirection };
+  }
+  public left(initPosition: { x: number; y: number; d: string }) {
+    const numericSidesOfWorld = ["n", "w", "s", "e"];
+    const numericTurnLeft = 1;
+    const newDirection =
+      numericSidesOfWorld[
+        numericSidesOfWorld.indexOf(initPosition.d) + numericTurnLeft
+      ] || numericSidesOfWorld[0];
+    return { ...initPosition, d: newDirection };
   }
 }
